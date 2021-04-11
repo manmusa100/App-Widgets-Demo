0ec81afd3b8cd01b9be9eb654cb29f052a6c99fb
Tsanguu <73098420+manmusa100@users.noreply.github.com>
2021-04-11 21:17:12 +0300

    minor changes

--- app/src/main/java/com/demo/appwidgetsdemo/NewAppWidget.java
+++ app/src/main/java/com/demo/appwidgetsdemo/NewAppWidget.java
@@ -7,10 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Button;
 import android.widget.RemoteViews;
-import android.widget.Toast;
-
 /**
  * Implementation of App Widget functionality.
  */
@@ -18,38 +15,31 @@ public class NewAppWidget extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             CharSequence widgetText = context.getString(R.string.appwidget_text);
             // Construct the RemoteViews object
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
             views.setTextViewText(R.id.appwidget_text, widgetText);
-
             // Create an Intent to launch MainActivity
             Intent intent = new Intent(context, MainActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
             // Get the layout for the App Widget and attach an on-click listener to the button
             views.setOnClickPendingIntent(R.id.button1, pendingIntent);
 
-            // Instruct the widget manager to update the widget
+            // Update the widget from the widget manager
             appWidgetManager.updateAppWidget(appWidgetId, views);
-
-            //updateAppWidget(context, appWidgetManager, appWidgetId);
         }
     }
-
     @Override
     public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
         super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
         Log.i("tag1","App Widget Resized");
     }
-
     @Override
     public void onEnabled(Context context) {
         // Enter relevant functionality for when the first widget is created
         Log.i("tag1","App Widget Enabled");
     }
-
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
