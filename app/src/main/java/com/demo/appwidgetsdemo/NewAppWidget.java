0e8c9ac947ea5287b2eea58c1bbdfbed9f54a0fd
Tsanguu <73098420+manmusa100@users.noreply.github.com>
2021-04-13 14:21:32 +0300

    variables renamed

--- app/src/main/java/com/demo/appwidgetsdemo/NewAppWidget.java
+++ app/src/main/java/com/demo/appwidgetsdemo/NewAppWidget.java
@@ -14,35 +14,35 @@ import android.widget.RemoteViews;
 public class NewAppWidget extends AppWidgetProvider {
 
     @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+    public void onUpdate(Context ctx, AppWidgetManager widgetManager, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
-            CharSequence widgetText = context.getString(R.string.appwidget_text);
+            CharSequence text = ctx.getString(R.string.widget_text);
             // Construct the RemoteViews object
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
-            views.setTextViewText(R.id.appwidget_text, widgetText);
-            // Create an Intent to launch MainActivity
-            Intent intent = new Intent(context, MainActivity.class);
-            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-            // Get the layout for the App Widget and attach an on-click listener to the button
-            views.setOnClickPendingIntent(R.id.button1, pendingIntent);
+            RemoteViews views = new RemoteViews(ctx.getPackageName(), R.layout.new_app_widget);
+            views.setTextViewText(R.id.widget_text, text);
+            // Launch MainActivity Intent
+            Intent i = new Intent(ctx, MainActivity.class);
+            PendingIntent penintent = PendingIntent.getActivity(ctx, 0, i, 0);
+            // Attach an on-click listener to the button
+            views.setOnClickPendingIntent(R.id.button1, penintent);
 
             // Update the widget from the widget manager
-            appWidgetManager.updateAppWidget(appWidgetId, views);
+            widgetManager.updateAppWidget(appWidgetId, views);
         }
     }
     @Override
-    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
-        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
-        Log.i("tag1","App Widget Resized");
-    }
-    @Override
-    public void onEnabled(Context context) {
+    public void onEnabled(Context ctx) {
         // Enter relevant functionality for when the first widget is created
         Log.i("tag1","App Widget Enabled");
     }
     @Override
-    public void onDisabled(Context context) {
+    public void onDisabled(Context ctx) {
         // Enter relevant functionality for when the last widget is disabled
         Log.i("tag1","App Widget Disabled");
+    }
+    @Override
+    public void onAppWidgetOptionsChanged(Context ctx, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        super.onAppWidgetOptionsChanged(ctx, appWidgetManager, appWidgetId, newOptions);
+        Log.i("tag1","Widget Resized");
     }
 }
